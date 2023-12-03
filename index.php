79aa8ea4dde656c5c34e400ed0daddb9dcd35b32
Süleyman Beyaz <116429151+suleymanbeyaz10@users.noreply.github.com>
2023-12-03 19:25:36 +0300

    index page extended

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>220757011-Süleyman Beyaz</title>
+    <link rel="stylesheet" href="<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">">
     <style>
         body {
             background-color: #222;
@@ -30,13 +31,14 @@
 
 <body>
     <header>
-        <h1>Hoş Geldiniz!</h1>
+        <h1>Hoş Geldiniz! </h1>
+        <h4>Biruni Üniversitesi Bilgisayar Programcılığı Vize Ödevi</h4>
     </header>
 
     <section id="intro-section">
-        <h2>SÜLEYMAN BEYAZ</h2>
-        <h5>220757011</h5>
-        <p>
+        <h1>SÜLEYMAN BEYAZ</h1>
+        <h2>220757011</h2>
+        <div class="container-sm" style="background-color: grey;">
             <?php echo "
             Ben Süleyman,
             Biruni Üniversitesi, Bilgisayar Programcılığı öğrencisiyim. Bilgisayar teknolojilerinin her türlüsüne olan merakımı yazılım alanında ilerletmek isteyen biriyim.
@@ -45,7 +47,7 @@
             bulabilirsiniz. İleriye dönük planlarımın arasında kurumsal projelere destek vererek ülkeme katkıda bulunmak
             ve bulunduğum konumun ihtiyaçlarını karşılamak önceliğimdir. ";
             ?>
-        </p>
+        </div>
     </section>
 </body>
 
